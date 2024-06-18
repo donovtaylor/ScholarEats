05dd80b7cf7ac5a3630ab46ca767d65f3456110f
DeadMartyr <Angelo.S.Arriaga@outlook.com>
2024-06-18 01:13:03 +0000

    Node Package Manager Attempt 1

---
+++ application/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const handlebars = require("express-handlebars");
+
+
+const app = express();
+
+app.use("/public", express.static(path.join(__dirname, "public")));
+
+app.use(function(req,res,next){
+    console.log(req.session);
+    if(req.session.user){
+        res.locals.isLoggedIn = true;
+        res.locals.user = req.session.user;
+    }
+    next();
+})
+
+app.use("/", indexRouter); // route middleware from ./routes/index.js
+
+app.use((req,res,next) => {
+  next(createError(404, `The route ${req.method} : ${req.url} does not exist.`));
+})
+
+module.exports = app;
