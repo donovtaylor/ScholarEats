4cda6d40b92be530612acf610c081c15f989deea
Edward McDonald <emcdonald1@mail.sfsu.edu>
2024-06-19 15:40:35 -0700

    Router testing

--- application/app.js
+++ application/app.js
@@ -1,16 +1,33 @@
 const express = require("express");
+const handlebars = require("express-handlebars");
+const path = require("path");
+const indexRouter = require("./routes/index");
+
 const app = express();
 
-//this is so images will load in our website
-app.use(express.static('website/pages'));
+// Set up Handlebars view engine
+app.engine("handlebars", handlebars());
+app.set("view engine", "handlebars");
 
-//serve the about page
-app.get("/", (req,res) => {
-  res.sendFile(__dirname + '/website/pages/index.html');
+// Serve static files from the "public" directory
+app.use("/public", express.static(path.join(__dirname, "public")));
+
+// Middleware to log session info and set response locals
+app.use(function(req, res, next) {
+    console.log(req.session);
+    if (req.session.user) {
+        res.locals.isLoggedIn = true;
+        res.locals.user = req.session.user;
+    }
+    next();
 });
 
-const port = 3000;
+// Use the router middleware from ./routes/index.js
+app.use("/", indexRouter);
 
-app.listen(port, () => {
-  console.log(`Express listening at http://0.0.0.0:${port}`);
+// 404 error handling middleware
+app.use((req, res, next) => {
+    next(createError(404, `The route ${req.method} : ${req.url} does not exist.`));
 });
+
+module.exports = app;
