0c412c5f3d2d1ec8ff7a55fd189adf3831f0c886
student <dvelasquez1@sfsu.edu>
2024-06-24 13:26:45 -0700

    simplified directory structure and got pictures working with handlebars

--- application/app.js
+++ application/app.js
@@ -4,25 +4,26 @@ const exphbs = require('express-handlebars'); // Import express-handlebars
 
 const app = express();
 
+
 // Serve static files from the 'website' directory (for existing HTML files)
 // app.use(express.static(path.join(__dirname, 'website/pages/images')));
 
 // Middleware to configure Handlebars
 app.engine('hbs', exphbs.engine({
   extname: 'hbs',
-  layoutsDir: path.join(__dirname, '/views/layouts'),
+  layoutsDir: path.join(__dirname, 'views/layouts'),
   partialsDir: path.join(__dirname, 'views/partials'),
   defaultLayout:'defaultLayout'
 }));
 
 app.set('view engine', 'hbs');
 
+
 // Example route using Handlebars (not converting existing HTML)
 app.get('/example', (req, res) => {
     // Render a Handlebars template
     res.render('example', { title: 'Handlebars Example' });
 });
-
 
 // Add more routes as needed for your existing HTML files
 app.get('/', (req, res) => {
@@ -34,11 +35,14 @@ app.get('/angelo', (req, res) => {
   res.render('about', {
     layout:'aboutPageLayout', 
     FName:'Angelo',
-    src:path.join(__dirname, 'website/pages/images/angelo.jpg'),
+    src:path.join(__dirname, 'public/images/angelo.jpg'),
     alt:'angelo.jpg',
     desc:'Angelo is a student at San Francisco State University. Has an aptitude for many hobbies ranging from sewing and clothesmaking to 3D modelling in Blender. Started at SFSU in January 2023 after finishing an enlistment in the United States Marine Corps and is expecting to graduate Decemeber 2024 with a B.S. in Computer Science and a Minor in Mathematics.'
   });
 });
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 // 404 Error handling
 app.use((req, res, next) => {
     res.status(404).send('404 Page Not Found');
