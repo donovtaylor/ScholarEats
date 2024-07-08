6789c3c3095670701703be6ce190f13e98bc5535
student <dvelasquez1@sfsu.edu>
2024-07-08 15:59:22 -0700

    did some work on warning message timeout

--- application/public/scripts/unfinished_button.js
+++ application/public/scripts/unfinished_button.js
@@ -1,12 +1,27 @@
+
 function handleClick(e) {
   if (e.target.className == "unfinished_button") {
-  warning.innerHTML = "unfinished button";
-  warning.style.display = 'block';
+    first = new Date();
+    warning.innerHTML = "unfinished button";
+    warning.style.display = 'block';
   }
-  setInterval(() => {
-    warning.style.display = 'none';
-  }, 5000);
+  
+  if (!timer1) {
+    timer1 = setTimeout(() => {
+      const second = new Date();
+      console.log(second - first);
+      console.log(timer1);
+      warning.style.display = 'none';
+    }, 3000);
+    timer1 = null;
+  }
 }
 
+var timer1 = null;
+var first = null;
+
+// target the warning banner in the navbar
 const warning = document.getElementById("content_warning");
+
+// add event listener to document in case there are multiple unfinished buttons
 document.addEventListener("click", handleClick);
