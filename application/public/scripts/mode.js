62edc2522e3d50a5129dc60c90c64b25f54df9a0
sderazo <sdiazera@gmail.com>
2024-07-22 08:53:10 -0700

    script for switching between light and dark mode

---
+++ application/public/scripts/mode.js
@@ -0,0 +1,20 @@
+function changeMode() {
+    const modeSelect = document.getElementById("modeSelect");
+    const modeStyleSheetLink = document.getElementById("modeStyleSheetLink");
+    const currentMode = sessionStorage.getItem("mode") || "login";
+
+    function setMode(modeName) {
+      modeStyleSheetLink.setAttribute("href", `/css/${modeName}.css`);
+    }
+
+    modeSelect.addEventListener("change", () => {
+      setMode(modeSelect.value);
+      sessionStorage.setItem("mode", modeSelect.value);
+
+    });
+
+    modeSelect.value = currentMode;
+    setMode(currentMode);
+  }
+
+  changeMode();
