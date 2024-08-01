0c0edbfd9eb9d1aa5efa168ee6d3253285fabc0e
student <dvelasquez1@sfsu.edu>
2024-08-01 01:13:12 -0700

    got dark mode hooked up to the back end, pending hooking it up to all the other pages

--- application/app.js
+++ application/app.js
@@ -31,26 +31,26 @@ const connection = require('./routes/db');
 const sessionStore = new MySQLStore({}, connection);
 
 app.use(session({
-	key: 'cookie_id',
-	secret: 'secret-key',
-	store: sessionStore,
-	resave: false,
-	saveUninitialized: false,
-	cookie: {
-		maxAge: 1000 * 60 * 60 * 24
-	}
+  key: 'cookie_id',
+  secret: 'secret-key',
+  store: sessionStore,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24
+  }
 }));
 
 app.use((req, res, next) => {
-	res.locals.isLoggedIn = req.session.user ? true : false;
-	if (res.locals.isLoggedIn) {
-		res.locals.isAdmin = req.session.user.role === 'admin';
-	} else {
-		res.locals.isAdmin = false;
-	}
-	//console.log('isLoggedIn:' + res.locals.isLoggedIn);
-	//console.log('isAdmin:' + res.locals.isAdmin);
-	next();
+  res.locals.isLoggedIn = req.session.user ? true : false;
+  if (res.locals.isLoggedIn) {
+    res.locals.isAdmin = req.session.user.role === 'admin';
+  } else {
+    res.locals.isAdmin = false;
+  }
+  //console.log('isLoggedIn:' + res.locals.isLoggedIn);
+  //console.log('isAdmin:' + res.locals.isAdmin);
+  next();
 });
 
 // Mount routes
@@ -65,10 +65,10 @@ app.use("/admin-tools", adminTools);
 
 // Middleware to configure Handlebars
 const hbs = exphbs.create({
-	layoutsDir: path.join(__dirname, 'views/layouts'),
-	partialsDir: path.join(__dirname, 'views/partials'),
-	defaultLayout: 'defaultLayout',
-	extname: 'hbs',
+  layoutsDir: path.join(__dirname, 'views/layouts'),
+  partialsDir: path.join(__dirname, 'views/partials'),
+  defaultLayout: 'defaultLayout',
+  extname: 'hbs',
 });
 
 app.engine('hbs', hbs.engine);
@@ -81,30 +81,35 @@ app.use(express.urlencoded({ extended: true }));
 app.use(flash());
 
 function debugMsg(input) { // Use this for debug messages, I got tired of doing a ton of if (debug) statements
-	if (debug) {
-		console.log(input);
-	}
+  if (debug) {
+    console.log(input);
+  }
 }
 
 //this piece of code is to pass the dropdown variables between routes
 app.locals.dropdownFilters = {
-	value: 'Filter', id: 'filter_options',
-	checkbox_option: ['Vegan', 'Gluten Free', 'Oven Required', 'Stove Required', 'Easy', 'Medium', 'Hard'],
-	radio_option: ['Calories Ascending', 'Calories Descending', 'Protein Ascending', 'Protein Descending', 'Fat Ascending', 'Fat Descending', 'Fiber Ascending', 'Fiber Descending']
+  value: 'Filter', id: 'filter_options',
+  checkbox_option: ['Vegan', 'Gluten Free', 'Oven Required', 'Stove Required', 'Easy', 'Medium', 'Hard'],
+  radio_option: ['Calories Ascending', 'Calories Descending', 'Protein Ascending', 'Protein Descending', 'Fat Ascending', 'Fat Descending', 'Fiber Ascending', 'Fiber Descending']
 };
 
 app.locals.dietaryRestrictions = {
-	value: 'Dietary Restrictions', id: 'dietary_restrictions',
-	checkbox_option: ['Vegan', 'Keto', 'Hala', 'Vegetarian', 'Pescatarian', 'Kosher']
+  value: 'Dietary Restrictions', id: 'dietary_restrictions',
+  checkbox_option: ['Vegan', 'Keto', 'Hala', 'Vegetarian', 'Pescatarian', 'Kosher']
 };
 
 app.locals.allergies = {
-	value: 'Allergies', id: 'allergies',
-	checkbox_option: ['Milk', 'Eggs', 'Fish', 'Crustacean Shellfish', 'Tree Nuts', 'Peanuts', 'Wheat', 'Soybeans']
+  value: 'Allergies', id: 'allergies',
+  checkbox_option: ['Milk', 'Eggs', 'Fish', 'Crustacean Shellfish', 'Tree Nuts', 'Peanuts', 'Wheat', 'Soybeans']
 };
 
 // Rendering recipes dynamically from the database
 app.get('/', async (req, res) => {
+
+
+  // const darkmode = req.session.user_info.modes == 'darkmode' ? 1 : 0;
+
+  // console.log(darkmode);
 
   // Recipes
   const recipeQuery = `
@@ -129,121 +134,143 @@ app.get('/', async (req, res) => {
         LIMIT 3
     `;
 
-	// Ingredients
-	const ingredientsQuery = `
+  // Ingredients
+  const ingredientsQuery = `
         SELECT s.ingredient_id, s.quantity, i.name, i.img_src
         FROM store s
         JOIN ingredient i ON s.ingredient_id = i.ingredient_id
         LIMIT 3
     `;
 
-	try {
-		const [recipeResults] = await connection.execute(recipeQuery);
+  try {
+    const [recipeResults] = await connection.execute(recipeQuery);
 
-		const [ingredientsResults] = await connection.execute(ingredientsQuery);
+    const [ingredientsResults] = await connection.execute(ingredientsQuery);
 
-		res.render('landingpage', {
-			style: ['default.css', 'landingpage.css'],
-			script: ['dropdown.js', 'unfinished_button.js', 'autocomplete.js', 'mode.js'],
-			dropdown1: app.locals.dropdownFilters,
-			recipes: recipeResults,
-			ingredients: ingredientsResults,
-			title: 'Landing Page'
-		});
-	} catch (err) {
-		console.error('Error fetching ingredients');
-		return res.status(500).send('Error fetching ingredients');
-	}
+    res.render('landingpage', {
+      style: ['default.css', 'landingpage.css'],
+      script: ['dropdown.js', 'unfinished_button.js', 'autocomplete.js', 'mode.js'],
+      dropdown1: app.locals.dropdownFilters,
+      recipes: recipeResults,
+      ingredients: ingredientsResults,
+      title: 'Landing Page'
+    });
+  } catch (err) {
+    console.error('Error fetching ingredients');
+    return res.status(500).send('Error fetching ingredients');
+  }
 });
 
 // Add more routes here as needed
 app.route('/about')
-	.get((req, res) => {
-		var searchInput = req.query.searchInput;
-		res.render('index', {
-			script: ['dropdown.js', 'unfinished_button.js', 'autocomplete.js'],
-			style: ['default.css'],
-			dropdown1: app.locals.dropdownFilters,
-			title: 'Team\'s about page',
-			header: 'Team\'s about page'
-		})
-	});
+  .get(async (req, res) => {
+    const userId = req.session.user.userId;
+
+    var styles = ['default.css'];
+
+    const query = `
+    SELECT ui.modes
+    FROM user_info ui
+    JOIN users u ON ui.user_id
+    = u.user_id
+    WHERE u.user_id = ?`;
+
+    try {
+      const [results] = await connection.execute(query, [userId]);
+      if (results.length > 0 && results[0].modes == 'darkmode') {
+        styles.push('darkmode.css');
+      }
+    } catch (err) {
+      console.error('Error fetching mode:', err);
+      return res.status(500).send('Error fetching mode');
+    }
+
+    //console.log(mode);
+
+    res.render('index', {
+      script: ['dropdown.js', 'unfinished_button.js', 'autocomplete.js'],
+      style: styles,
+      dropdown1: app.locals.dropdownFilters,
+      title: 'Team\'s about page',
+      header: 'Team\'s about page'
+    })
+  });
 
 // serve login page
 app.route('/login')
-	.get((req, res) => {
-		res.render('login', {
-			script: ['dropdown.js', 'unfinished_button.js', 'autocomplete.js'],
-			style: ['default.css', 'login.css'],
-			dropdown1: app.locals.dropdownFilters,
-			title: 'Login'
-		})
-	});
+  .get((req, res) => {
+    res.render('login', {
+      script: ['dropdown.js', 'unfinished_button.js', 'autocomplete.js'],
+      style: ['default.css', 'login.css'],
+      dropdown1: app.locals.dropdownFilters,
+      title: 'Login'
+    })
+  });
 
 // serve login page
 app.route('/adminlogin')
-	.get((req, res) => {
-		res.render('adminlogin', {
-			script: ['dropdown.js', 'unfinished_button.js', 'autocomplete.js', 'mode.js'],
-			style: ['default.css', 'login.css'],
-			dropdown1: app.locals.dropdownFilters,
-			title: 'Admin Login',
-			mode: 'login'
-		})
-	});
+  .get((req, res) => {
+    res.render('adminlogin', {
+      script: ['dropdown.js', 'unfinished_button.js', 'autocomplete.js', 'mode.js'],
+      style: ['default.css', 'login.css'],
+      dropdown1: app.locals.dropdownFilters,
+      title: 'Admin Login',
+      mode: 'login'
+    })
+  });
 
 
 // serve forgot password page
 app.get('/forgotpassword', (req, res) => {
-	res.render('forgotpassword', {
-		script: ['dropdown.js', 'unfinished_button.js', 'autocomplete.js'],
-		style: ['default.css', 'forgotpassword.css'],
-		dropdown1: app.locals.dropdownFilters,
-		title: 'Forgot Password'
-	});
+  res.render('forgotpassword', {
+    script: ['dropdown.js', 'unfinished_button.js', 'autocomplete.js'],
+    style: ['default.css', 'forgotpassword.css'],
+    dropdown1: app.locals.dropdownFilters,
+    title: 'Forgot Password'
+  });
 });
 
 // serve privacy policy page
 app.get('/privacy_policy', (req, res) => {
-	res.render('privacy_policy', {
-		script: ['dropdown.js', 'unfinished_button.js', 'autocomplete.js'],
-		style: ['default.css'],
-		dropdown1: app.locals.dropdownFilters,
-		title: 'Privacy Policy'
-	});
+  res.render('privacy_policy', {
+    script: ['dropdown.js', 'unfinished_button.js', 'autocomplete.js'],
+    style: ['default.css'],
+    dropdown1: app.locals.dropdownFilters,
+    title: 'Privacy Policy'
+  });
 });
 
 // serve terms of service page
 app.get('/termsofservice', (req, res) => {
-	res.render('termsofservice', {
-		script: ['dropdown.js', 'unfinished_button.js', 'autocomplete.js'],
-		style: ['default.css'],
-		dropdown1: app.locals.dropdownFilters,
-		title: 'Terms of Service'
-	});
+  res.render('termsofservice', {
+    script: ['dropdown.js', 'unfinished_button.js', 'autocomplete.js'],
+    style: ['default.css'],
+    dropdown1: app.locals.dropdownFilters,
+    title: 'Terms of Service'
+  });
 });
 
 // serve contact us page
 app.get('/contact_us', (req, res) => {
-	const teamMembers = [
-		{ name: 'Angelo Arriaga', src: 'images/angelo.jpg', alt: 'angelo.jpg', role: 'Team Lead', email: 'aarriaga1@sfsu.edu' },
-		{ name: 'Donovan Taylor', src: 'images/donovan.jpg', alt: 'donovan.jpg', role: 'Frontend Lead', email: 'dvelasquez1@sfsu.edu' },
-		{ name: 'Hancun Guo', src: 'images/hancun.jpg', alt: 'hancun.jpg', role: 'Frontend', email: 'hguo4@sfsu.edu' },
-		{ name: 'Edward Mcdonald', src: 'images/edward.jpg', alt: 'edward.jpg', role: 'Backend Lead', email: 'emcdonald1@sfsu.edu' },
-		{ name: 'Karl Carsola', src: 'images/karl.jpg', alt: 'karl.jpg', role: 'Backend', email: 'kcarsola@mail.sfsu.edu' },
-		{ name: 'Sai Bavisetti', src: 'images/sai.jpg', alt: 'sai.jpg', role: 'Database', email: 'sbavisetti@sfsu.edu' },
-		{ name: 'Maeve Fitzpatrick', src: 'images/maeve.jpg', alt: 'maeve.jpg', role: 'Docs-Editor', email: 'mfitzpatrick@sfsu.edu' },
-		{ name: 'Sabrina Diaz-Erazo', src: 'images/sabrina.jpg', alt: 'sabrina.jpg', role: 'GitHub Master', email: 'sdiazerazo@sfsu.edu' },
-		{ name: 'Tina Chou', role: 'Frontend', src: 'images/tina.jpg', alt: 'tina.jpg', email: 'ychou@sfsu.edu' }
-	];
-	// add styling to contact_us page
-	res.render('contact_us', {
-		style: ['default.css'],
-		dropdown1: app.locals.dropdownFilters,
-		script: ['dropdown.js', 'unfinished_button.js', 'autocomplete.js'],
-		teamMembers,
-		title: 'Contact Us'
-	});
+  const teamMembers = [
+    { name: 'Angelo Arriaga', src: 'images/angelo.jpg', alt: 'angelo.jpg', role: 'Team Lead', email: 'aarriaga1@sfsu.edu' },
+    { name: 'Donovan Taylor', src: 'images/donovan.jpg', alt: 'donovan.jpg', role: 'Frontend Lead', email: 'dvelasquez1@sfsu.edu' },
+    { name: 'Hancun Guo', src: 'images/hancun.jpg', alt: 'hancun.jpg', role: 'Frontend', email: 'hguo4@sfsu.edu' },
+    { name: 'Edward Mcdonald', src: 'images/edward.jpg', alt: 'edward.jpg', role: 'Backend Lead', email: 'emcdonald1@sfsu.edu' },
+    { name: 'Karl Carsola', src: 'images/karl.jpg', alt: 'karl.jpg', role: 'Backend', email: 'kcarsola@mail.sfsu.edu' },
+    { name: 'Sai Bavisetti', src: 'images/sai.jpg', alt: 'sai.jpg', role: 'Database', email: 'sbavisetti@sfsu.edu' },
+    { name: 'Maeve Fitzpatrick', src: 'images/maeve.jpg', alt: 'maeve.jpg', role: 'Docs-Editor', email: 'mfitzpatrick@sfsu.edu' },
+    { name: 'Sabrina Diaz-Erazo', src: 'images/sabrina.jpg', alt: 'sabrina.jpg', role: 'GitHub Master', email: 'sdiazerazo@sfsu.edu' },
+    { name: 'Tina Chou', role: 'Frontend', src: 'images/tina.jpg', alt: 'tina.jpg', email: 'ychou@sfsu.edu' }
+  ];
+  // add styling to contact_us page
+  res.render('contact_us', {
+    style: ['default.css'],
+    dropdown1: app.locals.dropdownFilters,
+    script: ['dropdown.js', 'unfinished_button.js', 'autocomplete.js'],
+    teamMembers,
+    title: 'Contact Us'
+  });
 });
 
 // serve registration page
@@ -258,9 +285,14 @@ app.get('/register', (req, res) => {
 
 // add dark mode button here
 app.get('/accountmanagement', (req, res) => {
+
+  const styles = ['default.css', 'accountmanagement.css']
+
+
+
   res.render('accountManagement', {
     script: ['dropdown.js', 'unfinished_button.js', 'autocomplete.js', 'mode.js'],
-    style: ['default.css', 'accountmanagement.css'],
+    style: styles,
     dropdown1: app.locals.dropdownFilters,
     dropdown2: app.locals.dietaryRestrictions,
     dropdown3: app.locals.allergies,
@@ -270,11 +302,11 @@ app.get('/accountmanagement', (req, res) => {
 
 // 404 Error handling
 app.use((req, res, next) => {
-	res.status(404).send('404 Page Not Found');
+  res.status(404).send('404 Page Not Found');
 });
 
 // Start server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-	console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
